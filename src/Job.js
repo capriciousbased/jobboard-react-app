efc06d1b6a0daef46bbbc9684cdc34e3ae7c71ad
capriciousbased <mdanisulhoquekhan@gmail.com>
2021-04-22 13:57:07 +0200

    initial commit

---
+++ src/Job.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Job = () => {
+    return (
+        <div className="job">
+            
+        </div>
+    )
+}
+
+export default Job
---
+++ src/Jobs.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Typography from '@material-ui/core/Typography';
+
+const Jobs = () => {
+    return (
+        <div className="jobs">
+           <Typography variant="h1">
+               Entry Levele Software Job
+           </Typography> 
+        </div>
+    )
+}
+
+export default Jobs
